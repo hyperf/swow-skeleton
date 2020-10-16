8579d89c9a54edf412945e40a608ad6aff5f753b
李铭昕 <715557344@qq.com>
2020-10-16 10:54:57 +0800

    Optimized code

--- config/autoload/server.php
+++ config/autoload/server.php
@@ -11,7 +11,6 @@ declare(strict_types=1);
  */
 use Hyperf\Server\Server;
 use Hyperf\Server\SwooleEvent;
-use Swow\Socket;
 
 return [
     'type' => Hyperf\Server\SwowServer::class,
@@ -21,7 +20,6 @@ return [
             'type' => Server::SERVER_HTTP,
             'host' => '0.0.0.0',
             'port' => 9501,
-            'sock_type' => Socket::TYPE_TCP,
             'callbacks' => [
                 SwooleEvent::ON_REQUEST => [Hyperf\HttpServer\Server::class, 'onRequest'],
             ],
